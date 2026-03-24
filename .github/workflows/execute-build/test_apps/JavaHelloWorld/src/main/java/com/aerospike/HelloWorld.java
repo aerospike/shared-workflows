a47c373a28ca1930e417a4a43383d1b95ff5a18f
Joe Martin <jmartin@aerospike.com>
2026-03-24 16:48:19 -0700

    chore(workflows): add npm and Java support to example workflows

---
+++ .github/workflows/execute-build/test_apps/JavaHelloWorld/src/main/java/com/aerospike/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.aerospike;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello from Aerospike HelloWorld!");
+    }
+}
---
+++ .github/workflows/execute-build/test_apps/NpmHelloWorld/index.js
@@ -0,0 +1,1 @@
+console.log("Hello from @aerospike/hello-world!");
